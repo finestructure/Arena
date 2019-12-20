48bf88acaf11b2259f99ebd29c87d504888cd02c
Sven A. Schmidt <sas@finestructure.co>
2019-12-20 15:38:07 +0100

    Bring back testing

---
+++ Tests/SPMPlaygroundTests/SPMPlaygroundTests.swift
@@ -0,0 +1,49 @@
+import XCTest
+import class Foundation.Bundle
+
+
+final class SPMPlaygroundTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+
+        // Some of the APIs that we use below are available in macOS 10.13 and above.
+        guard #available(macOS 10.13, *) else {
+            return
+        }
+
+        let fooBinary = productsDirectory.appendingPathComponent("spm-playground")
+
+        let process = Process()
+        process.executableURL = fooBinary
+        process.arguments = ["--version"]
+
+        let pipe = Pipe()
+        process.standardOutput = pipe
+
+        try process.run()
+        process.waitUntilExit()
+
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8)!
+
+        XCTAssertEqual(output, "undefined version: run 'make version' to update\n")
+    }
+
+    /// Returns path to the built products directory.
+    var productsDirectory: URL {
+      #if os(macOS)
+        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
+            return bundle.bundleURL.deletingLastPathComponent()
+        }
+        fatalError("couldn't find the products directory")
+      #else
+        return Bundle.main.bundleURL
+      #endif
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
