71cb424c7509a4c98703f3e5bc8c5a2c744f10b2
Sven A. Schmidt <sas@finestructure.co>
2020-03-31 08:22:49 +0200

    Split up integration tests

--- Tests/ArenaTests/IntegrationTests.swift
+++ Tests/ArenaTests/IntegrationTests.swift
@@ -10,108 +10,34 @@ import XCTest
 
 
 class IntegrationTests: XCTestCase {
-    
-    func test_ArenaTest_luna() throws {
-//        try XCTSkipUnless(ProcessInfo().hostName == "luna.local", "fails on CI, only run locally")
-
-        let output = OutputListener()
-        output.openConsolePipe()
-        
-        let arena = try Arena.parse([
-            "https://github.com/finestructure/ArenaTest@0.0.3",
-            "--name=ArenaIntegrationTest",
-            "--force",
-            "--skip-open"])
-        try arena.run()
-        
-        let expectation = """
-                ➡️  Package: https://github.com/finestructure/ArenaTest @ exact(0.0.3)
-                🔧 Resolving package dependencies ...
-                📔 Libraries found: ArenaTest
-                🔨 Building package dependencies ...
-                ✅ Created project in folder 'ArenaIntegrationTest'
-                Run
-                  open ArenaIntegrationTest/ArenaIntegrationTest.xcworkspace
-                to open the project in Xcode
-
-                """
-        let predicate = NSPredicate { _,_  in
-            output.contents == expectation
-        }
-        let exp = XCTNSPredicateExpectation(predicate: predicate, object: nil)
-        wait(for: [exp], timeout: 10)
-        XCTAssertEqual(output.contents, expectation)
-        
-        output.closeConsolePipe()
-    }
 
     func test_ArenaTest() throws {
-//        try XCTSkipUnless(ProcessInfo().hostName == "luna.local", "fails on CI, only run locally")
-
-        let arena = try Arena.parse([
+        let dependencies = [
             "https://github.com/finestructure/ArenaTest@0.0.3",
-            "--name=ArenaIntegrationTest",
-            "--force",
-            "--skip-open"])
+            "finestructure/parser",
+            "finestructure/gala",
+        ]
 
-        let exp = self.expectation(description: "exp")
+        try dependencies.forEach { dep in
+            let arena = try Arena.parse([
+                dep,
+                "--name=ArenaIntegrationTest",
+                "--force",
+                "--skip-open"])
 
-        let progress: ProgressUpdate = { stage, _ in
-            print("progress: \(stage)")
-            if stage == .completed {
-                exp.fulfill()
+            let exp = self.expectation(description: "exp")
+
+            let progress: ProgressUpdate = { stage, _ in
+                print("progress: \(stage)")
+                if stage == .completed {
+                    exp.fulfill()
+                }
             }
+
+            try arena.run(progress: progress)
+
+            wait(for: [exp], timeout: 10)
         }
-
-        try arena.run(progress: progress)
-
-        wait(for: [exp], timeout: 10)
-    }
-
-    func test_Gen() throws {
-        try XCTSkipUnless(ProcessInfo().hostName == "luna.local", "fails on CI, only run locally")
-
-        let arena = try Arena.parse([
-            "https://github.com/pointfreeco/swift-gen@0.2.0",
-            "--name=ArenaIntegrationTest",
-            "--force",
-            "--skip-open"])
-
-        let exp = self.expectation(description: "exp")
-
-        let progress: ProgressUpdate = { stage, _ in
-            print("progress: \(stage)")
-            if stage == .completed {
-                exp.fulfill()
-            }
-        }
-
-        try arena.run(progress: progress)
-
-        wait(for: [exp], timeout: 10)
-    }
-
-    func test_git_protocol() throws {
-        try XCTSkipUnless(ProcessInfo().hostName == "luna.local", "fails on CI, only run locally")
-
-        let arena = try Arena.parse([
-            "git@github.com:finestructure/ArenaTest@0.0.3",
-            "--name=ArenaIntegrationTest",
-            "--force",
-            "--skip-open"])
-
-        let exp = self.expectation(description: "exp")
-
-        let progress: ProgressUpdate = { stage, _ in
-            print("progress: \(stage)")
-            if stage == .completed {
-                exp.fulfill()
-            }
-        }
-
-        try arena.run(progress: progress)
-
-        wait(for: [exp], timeout: 10)
     }
 
 }
