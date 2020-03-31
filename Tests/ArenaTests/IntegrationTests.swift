6d42e3c75ba35e83d649fa6bb452c96a548dc920
Sven A. Schmidt <sas@finestructure.co>
2020-03-31 10:10:12 +0200

    Suspect latest release requests aren't going out

--- Tests/ArenaTests/IntegrationTests.swift
+++ Tests/ArenaTests/IntegrationTests.swift
@@ -22,7 +22,7 @@ class IntegrationTests: XCTestCase {
             // "https://github.com/pointfreeco/swift-gen",  // https://github.com/finestructure/Arena/issues/43
             "https://github.com/apple/swift-argument-parser",
             "https://github.com/davedelong/time",
-            "https://github.com/alamofire/alamofire",
+            "https://github.com/alamofire/alamofire@from:5.0.0",
         ]
 
         try dependencies.forEach { dep in
