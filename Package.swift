02a57686f5d9f2ddc71f999d8c17cf51ffd974a9
Sven A. Schmidt <sas@finestructure.co>
2020-06-11 08:54:20 +0200

    Add platforms

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "Arena",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .executable(name: "arena", targets: ["ArenaCLI"]),
         .library(name: "ArenaCore", type: .dynamic, targets: ["ArenaCore"])
