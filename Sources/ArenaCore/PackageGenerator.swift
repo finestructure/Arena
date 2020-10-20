de54e8a65d40c8589dd2e052aae286591ab8c0e4
Sven A. Schmidt <sas@finestructure.co>
2020-10-20 15:24:08 +0200

    Cleanup

--- Sources/ArenaCore/PackageGenerator.swift
+++ Sources/ArenaCore/PackageGenerator.swift
@@ -62,34 +62,9 @@ extension PackageGenerator {
 }
 
 
-struct PlatformVersion {
-    var major: Int
-    var minor: Int
-
-    init?(string: String) {
-        let parts = string.split(separator: ".")
-            .map(String.init)
-            .compactMap(Int.init)
-        guard parts.count == 2 else { return nil }
-        major = parts[0]
-        minor = parts[1]
-    }
-}
-
-
-extension PlatformVersion: Comparable {
-    static func < (lhs: PlatformVersion, rhs: PlatformVersion) -> Bool {
-        if lhs.major != rhs.major { return lhs.major < rhs.major }
-        return lhs.minor < rhs.minor
-    }
-}
-
-
 func max(_ a: Manifest.Platform, _ b: Manifest.Platform) -> Manifest.Platform {
     precondition(a.platformName == b.platformName)
-    let va = PlatformVersion(string: a.version)
-    let vb = PlatformVersion(string: b.version)
-    switch (va, vb) {
+    switch (PlatformVersion(string: a.version), PlatformVersion(string: b.version)) {
         case (.none, .none):
             fatalError("both platform versions are invalid: \(a.version), \(b.version)")
         case (.some, .none):
