7ff36d572df64a28b990e49455214c7d5a64847f
Sven A. Schmidt <sas@finestructure.co>
2023-03-08 11:53:13 +0100

    Skip test on outdated GH actions macos platform tooling

--- Tests/ArenaTests/RegressionTests.swift
+++ Tests/ArenaTests/RegressionTests.swift
@@ -12,6 +12,7 @@ final class RegressionTests: XCTestCase {
         Current = .mock
     }
 
+#if swift(>=5.7)  // test can only run on 5.7+ due to manifest tools-version
     func test_issue_90() throws {
         // https://github.com/finestructure/Arena/issues/90
         // macCatalyst platform in manifest
@@ -19,5 +20,6 @@ final class RegressionTests: XCTestCase {
                        [.macos("12.0"), .macCatalyst("15.0"), .ios("15.0"), .watchos("8.0"), .tvos("15.0")])
 
     }
+#endif
 
 }
