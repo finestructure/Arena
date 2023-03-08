b28fd38ebb3e584f6d0fee2cadf1b42d301dc646
Sven A. Schmidt <sas@finestructure.co>
2023-03-08 11:44:43 +0100

    Add support for macCatalyst platform

---
+++ Tests/ArenaTests/RegressionTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+
+@testable import ArenaCore
+
+import Path
+
+
+final class RegressionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        Current = .mock
+    }
+
+    func test_issue_90() throws {
+        // https://github.com/finestructure/Arena/issues/90
+        // macCatalyst platform in manifest
+        XCTAssertEqual(try getPackageInfo(in: fixturesDirectory/"Issue-90").platforms,
+                       [.macos("12.0"), .macCatalyst("15.0"), .ios("15.0"), .watchos("8.0"), .tvos("15.0")])
+
+    }
+
+}
